92e6e90a92dd1d26f2de8186c5dd047a804e0f7d
BradleyLamitie <Bradley.Lamitie1@marist.edu>
2017-10-23 13:45:12 -0400

    Added a few more sprites for the overworld
    added the open clock tower doors and added the tilemap files made using
    Tiled

---
+++ Genesis_Grassland_tileset.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Genesis_grassland_tiles" tilewidth="25" tileheight="25" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_bottom.png"/>
+ </tile>
+ <tile id="1">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_bottom_corner.png"/>
+ </tile>
+ <tile id="2">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_corner.png"/>
+ </tile>
+ <tile id="3">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_top.png"/>
+ </tile>
+ <tile id="4">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_top_corner.png"/>
+ </tile>
+ <tile id="5">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_top_edge.png"/>
+ </tile>
+ <tile id="6">
+  <image width="25" height="25" source="Genesis_Sprites/Cliff_wall.png"/>
+ </tile>
+ <tile id="7">
+  <image width="25" height="25" source="Genesis_Sprites/Clocktower_door.png"/>
+ </tile>
+ <tile id="8">
+  <image width="25" height="25" source="Genesis_Sprites/Clocktower_door_open.png"/>
+ </tile>
+ <tile id="9">
+  <image width="25" height="25" source="Genesis_Sprites/Grass_cut.png"/>
+ </tile>
+ <tile id="10">
+  <image width="25" height="25" source="Genesis_Sprites/Grass_uncut.png"/>
+ </tile>
+ <tile id="11">
+  <image width="25" height="25" source="Genesis_Sprites/Ground_grass.png"/>
+ </tile>
+ <tile id="12">
+  <image width="25" height="25" source="Genesis_Sprites/Rock_exploded.png"/>
+ </tile>
+ <tile id="13">
+  <image width="25" height="25" source="Genesis_Sprites/Rock_turtle.png"/>
+ </tile>
+ <tile id="14">
+  <image width="25" height="25" source="Genesis_Sprites/Rock_unexploded.png"/>
+ </tile>
+</tileset>
